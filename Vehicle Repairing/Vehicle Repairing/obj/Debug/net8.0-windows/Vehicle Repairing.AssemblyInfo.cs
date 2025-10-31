159b12db52d4e6b638dc2d2e6d6b7c3d81d4c873
Diasmyri <diasmayri15@gmail.com>
2025-10-31 16:59:31 +0700

    adding test

--- Vehicle Repairing/Vehicle Repairing/obj/Debug/net8.0-windows/Vehicle Repairing.AssemblyInfo.cs
+++ Vehicle Repairing/Vehicle Repairing/obj/Debug/net8.0-windows/Vehicle Repairing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vehicle Repairing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395f665167e5fb90af487c375b00e1b2c52b3f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf96301a698acf269b28ce2cb5433e3acd91406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vehicle Repairing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vehicle Repairing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
